f4c7b2c09642c33d9c89065449bf449fcec3675c
Ranjeet Girajaprasad Gupta <121397182+ranjeetgupta1995@users.noreply.github.com>
2023-10-29 21:40:46 +0530

    b30s4a1

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const { connection } = require("./db");
 const { userRouter } = require("./routes/users.routes");
 const { booksRouter } = require("./routes/books.routes");
+const cors = require("cors")
 require("dotenv").config()
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Home Page!!!!!")
